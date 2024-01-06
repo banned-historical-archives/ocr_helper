d2cb96c2199aa4eb1707950eed6a1572b15a4c72
Your Name <you@example.com>
2024-01-07 02:00:03 +0800

    fix:tags

--- build_tags.ts
+++ build_tags.ts
@@ -22,19 +22,21 @@ import { get_tags } from './get_tags';
 let [_, __, parsed_article_dir, tags_dir] = process.argv;
 
 (async () => {
-  const f1_list = (await fs.readdir(parsed_article_dir)).filter(i => !i.startsWith('.') && i !== 'README.md')
-  for (const f1 of f1_list) {
-    const book_list = await fs.readdir(join(parsed_article_dir, f1))
+  const p1 = (await fs.readdir(parsed_article_dir)).filter(i => !i.startsWith('.') && i !== 'README.md')
+  for (const prefix1 of p1) {
+    const book_list = await fs.readdir(join(parsed_article_dir, prefix1))
     let i = 0;
     for (const book of book_list) {
       ++i;
       console.log(book, `${i}/${book_list.length}`);
-      const article_list = (await fs.readdir(join(parsed_article_dir, f1, book))).filter(i => !i.endsWith('.bookinfo'));
-      for (const article of article_list) {
-        const data = JSON.parse(fs.readFileSync(join(parsed_article_dir, f1, book, article)).toString()) as ParserResult;
-        const target_dir = join(tags_dir, f1, book);
-        fs.ensureDirSync(target_dir);
-        fs.writeFileSync(join(target_dir, article), JSON.stringify(get_tags(data)));
+      const prefix2_list = (await fs.readdir(join(parsed_article_dir, prefix1, book))).filter(i => !i.endsWith('.bookinfo'));
+      for (const prefix2 of prefix2_list) {
+        for (const article of await fs.readdir(join(parsed_article_dir, prefix1, book, prefix2))) {
+          const data = JSON.parse(fs.readFileSync(join(parsed_article_dir, prefix1, book, article)).toString()) as ParserResult;
+          const target_dir = join(tags_dir, prefix1, book);
+          fs.ensureDirSync(target_dir);
+          fs.writeFileSync(join(target_dir, article), JSON.stringify(get_tags(data)));
+        }
       }
     }
   }
