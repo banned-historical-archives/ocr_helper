fe6362a47f9626ffc80df01bd3d7d5b364905b67
Your Name <you@example.com>
2024-01-10 22:57:50 +0800

    fix:tags

--- get_tags.ts
+++ get_tags.ts
@@ -1,4 +1,47 @@
-import { ParserResult, TagType } from "./types";
+import { ArticleType, ParserResult, TagType } from "./types";
+function multi_match(a: RegExp[], b: string[]) {
+  return a.reduce(
+    (m, i) => b.reduce((n, j) => n || i.test(j), false) || m,
+    false,
+  );
+}
+
+export function get_article_types(parser_result: ParserResult) {
+  const { title, description } = parser_result;
+  const res: ArticleType[] = [];
+  if (
+    multi_match(
+      [/讲演/, /演讲/, /演说/, /讲话/, /发言/, /座谈会/, /开幕/, /闭幕/],
+      [title, description],
+    )
+  ) {
+    res.push(ArticleType.lecture);
+  }
+  if (multi_match([/谈话/, /对话/], [title, description])) {
+    res.push(ArticleType.talk);
+  }
+  if (multi_match([/的信[。]?$/], [title, description])) {
+    res.push(ArticleType.mail);
+  }
+  if (multi_match([/宣言/, /声明/, /布告/, /公开信/], [title, description])) {
+    res.push(ArticleType.declaration);
+  }
+  if (
+    multi_match([/指令/, /命令/, /指示/, /通知/, /通报/], [title, description])
+  ) {
+    res.push(ArticleType.instruction);
+  }
+  if (multi_match([/评论/, /批语/, /批注/, /批示/], [title, description])) {
+    res.push(ArticleType.comment);
+  }
+  if (multi_match([/电报/, /通讯/], [title, description])) {
+    res.push(ArticleType.telegram);
+  }
+  if (!res.length) {
+    res.push(ArticleType.writings);
+  }
+  return res;
+}
 
 export function get_tags(
     parser_result: ParserResult,
@@ -545,5 +588,5 @@ export function get_tags(
   
     const subjects_results = [...provice_results, ...event_results];
   
-    return [...character_results, ...subjects_results];
+    return [...character_results, ...subjects_results, ...get_article_types(parser_result).map(i => ({type: TagType.articleType, name: i}))];
 }
