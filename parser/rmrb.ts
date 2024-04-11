6fd31cbbe54b4c29bdcb2c7e29a6a930df6735b5
Your Name <you@example.com>
2024-04-11 22:13:07 +0800

    fix:rmrb

--- parser/rmrb.ts
+++ parser/rmrb.ts
@@ -17,58 +17,33 @@ import {
 } from '../types';
 import { merge_to_lines } from '../utils';
 import { execSync } from 'child_process';
+import fs from 'fs';
 
 export default async function (
-  dirPathOrFilePath: string,
+  path: string,
 ): Promise<ParserResult[]> {
-  const year_start = 1946;
-  const year_end = 1958;
   const res: ParserResult[] = [];
-  return [];
-  // TODO 数量太大，构建时磁盘空间不足
-  for (let i = year_start; i <= year_end; ++i) {
-    for (let j = year_start === 1946 ? 5 : 1; j <= 12; ++j) {
-      const dir = `${i}年${j > 9 ? j : '0' + j}月`;
-      try {
-        execSync(`7z x -y ${dir}.7z`, {
-          cwd: dirPathOrFilePath,
-        });
-      } catch (e) {}
-      const files = readdirSync(join(dirPathOrFilePath, dir));
-      for (const file of files) {
-        const content = readFileSync(
-          join(dirPathOrFilePath, dir, file),
-        )
-          .toString()
-          .split('\n');
+  const root = path;
+  for (let year of fs.readdirSync((`${root}`))) {
+    for (let month of fs.readdirSync((`${root}/${year}`))) {
+      for (let article of fs.readdirSync((`${root}/${year}/${month}`))) {
+        const json = JSON.parse(
+          fs.readFileSync((`${root}/${year}/${month}/${article}`)).toString(),
+        );
         res.push({
-          title: content[0].substring(4),
-          dates: [
-            {
-              year: parseInt(content[2].split('-')[0]),
-              month: parseInt(content[2].split('-')[1]),
-              day: parseInt(content[2].split('-')[2]),
-            },
-          ],
-          authors: content[1].split('　').filter((i) => i),
-          parts: content
-            .slice(6)
-            .map((i) => i.trim())
-            .filter((i) => i)
-            .map((i) => ({
-              type: ContentType.paragraph,
-              text: i,
-            })),
-          comments: [],
-          page_end: 0,
-          page_start: 0,
+          title: json.title,
+          authors: json.authors,
+          dates: json.dates,
           is_range_date: false,
+          parts: json.parts,
+          comments: [],
           comment_pivots: [],
           description: '',
+          page_start: 1,
+          page_end: 1,
         });
       }
     }
   }
-
   return res;
 }
